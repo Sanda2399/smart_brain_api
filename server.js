9f4ada9bce53e68451cb188183278b977a9d690d
Brandon Mitchell <83912617+Sanda2399@users.noreply.github.com>
2021-11-15 10:45:16 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ const db = require('knex')({
     client: 'pg',
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false,
-        },
+        ssl: true
     },
 });
 const register = require('./Controllers/register');
